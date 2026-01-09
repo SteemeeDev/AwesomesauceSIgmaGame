a45010674c01aea7bb258e452b11e0cb10c35dd4
JJoe <48448023+JJoeDev@users.noreply.github.com>
2026-01-09 13:52:29 +0100

    player code & move animation

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Player : Entity
 {
     [SerializeField] private float MaxMoveSpeed;
+    [SerializeField] private float RotationSpeed;
+    [SerializeField] private float RotationThreshold = 5;
+    [SerializeField] private Transform PlayerMeshTransform;
 
     private Rigidbody rb;
 
@@ -14,7 +17,15 @@ public class Player : Entity
 
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * maxMoveSpeed;
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 movement = input.normalized * maxMoveSpeed;
+
+        if(input.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(input, Vector3.up);
+
+            PlayerMeshTransform.rotation = Quaternion.Slerp(PlayerMeshTransform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+        }
 
         rb.velocity = movement;
     }
